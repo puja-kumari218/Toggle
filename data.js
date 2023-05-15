c54e1440ca852a20f10b771370cd0343c22678b4
puja kumari <pkumari41038@gmail.com>
2023-05-15 19:45:05 +0530

    img

---
+++ data.js
@@ -0,0 +1,32 @@
+const articles = [
+    {
+      id: 1,
+      title: 'the WET Codbase',
+      date: new Date(2023, 8, 5),
+      length: 11,
+      snippet: `Java is to JavaScript what car is to Carpet`,
+    },
+    {
+      id: 2,
+      title: 'goodby, clean code',
+      date: new Date(2023, 8, 21),
+      length: 5,
+      snippet: `“First, solve the problem. Then, write the code.” `,
+    },
+    {
+      id: 3,
+      title: 'my decade in review',
+      date: new Date(2001, 18, 12),
+      length: 5,
+      snippet: `“Optimism is an occupational hazard of programming: feedback is the treatment.“ 
+      `,
+    },
+    {
+      id: 4,
+      title: 'what are the react team principles',
+      date: new Date(2002, 18, 6),
+      length: 5,
+      snippet: `“When to use iterative development? You should use iterative development only on projects that you want to succeed.”
+      `,
+    },
+  ];
